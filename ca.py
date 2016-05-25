4f1bf085c876a7601e03b965a8552b05e507b6d1
id4zs2008 <id4zs2008@gmail.com>
2016-05-25 15:07:48 +0800

    Update ca.py

--- ca.py
+++ ca.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
+"""
+Calculate conditional activity between pairs of residue PHIs or PSIs
+as implemented in Milo Lin JACS 2016
+"""
 import numpy as np
 from sklearn.metrics import mutual_info_score
 import MDAnalysis
 import os
 import re
+import math
+import sys
 
-"""
-Calculate conditional activity as implemented in Milo Lin JACS 2016"""
 
-"""transition time function T(X, i) to get the time of Nth transition 
-of a time series X, unit: 100 ps"""
+"""get a list of transition time points in sequential order"""
 def trans_time(X):
     t_t = []
     """increase 1D neighbor search radius nbr to filter out thermal fluctuations,
         assuming symmetric peaks, for unsymmetric case need left nbr and right nbr"""
-    nbr = 5
-    for i in range(1, len(X) - nbr):
+    nbr = 4
+    for i in range(0 + nbr, len(X) - nbr):
         peak = 1
         for j in range(1,nbr+1):
             if X[i] < X[i - j] or X[i] < X[i + j]:
@@ -24,7 +27,40 @@ def trans_time(X):
             t_t.append(i+1)
     return t_t
 
+
+"""
+transition time function T(X, n) to get the time of
+N th transition of a time series X time unit is 100 picosecond
+"""
+def trans_time_n(X, n):
+    T = trans_time(X)
+    return trans_time(X)[n-1]
+
+
+"""
+waiting time function W(X, t) to get the time interval
+from t until the next transition/peak of X
+"""
+def wait_time(X, t):
+    if t < 0 :
+        sys.exit("Error: time needs to be a positive number")
+    wait = 0
+    T = trans_time(X)
+    for i in range(0,len(T)-1):
+        if t >= T[i] and t <= T[i+1]:
+            wait = T[i+1] - t
+            break
+        elif t <= T[i]:
+            wait = T[i] - t
+            break
+    return wait
+
+
+
 X = np.loadtxt('dih-sample', usecols = (0,), unpack = True)
 T = trans_time(X)
 for i in range(0,len(T)):
     print T[i]
+#print "#####"
+#wait = wait_time(X, 93)
+#print wait
